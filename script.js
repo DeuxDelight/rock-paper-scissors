3ef7ed83c1fa4b5172f016545170699ceafe5a00
DeuxDelight <miguel.chavez65@gmail.com>
2023-01-30 22:52:22 -0700

    added to playGame() but throws errors

--- script.js
+++ script.js
@@ -5,30 +5,38 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    
+    // if(playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors'){
+    //     return 'Error: choose a valid response';
+    // }
 
     if(playerSelection === 'rock' && computerSelection === 'paper'){
+        computerScore++;
         return "You Lose! Paper beats Rock";
     }
     else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        playerScore++;
         return "You Win! Rock beats scissors";
     }
     else if(playerSelection === 'rock' && computerSelection === 'rock'){
         return "Its a draw!";
     }
     if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        computerScore++;
         return "You Lose! Scissors beats Paper";
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock'){
+        playerScore++;
         return "You Win! Paper beats Rock";
     }
     else if(playerSelection === 'paper' && computerSelection === 'paper'){
         return "Its a draw!";
     }
     if(playerSelection === 'scissors' && computerSelection === 'rock'){
+        computerScore++;
         return "You Lose! Rock beats Scissors";
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        playerScore++;
         return "You Win! Scissors beats Paper";
     }
     else if(playerSelection === 'scissors' && computerSelection === 'scissors'){
@@ -37,14 +45,24 @@ function playRound(playerSelection, computerSelection){
 }
 
 function game(){
+    const computerSelection = getComputerChoice();
+    let computerScore = 0;
+    let playerScore = 0;
+    
     for(let i = 0; i < 5; i++){
-        
+        let playerSelection = prompt('Rock, Paper, or Scissors?');
+        playerSelection = playerSelection.toLowerCase();
+        console.log(`Player chooses ${playerSelection}`);
+        console.log(`Computer chooses ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
     }
+    if(playerScore > computerScore){
+        console.log('After 5 rounds, you beat the computer!')
+    }
+    else{
+        console.log('After 5 rounds, you lost against the computer!')
+    }
+    
 }
 
-let playerSelection = 'SCISSORS';
-const computerSelection = getComputerChoice();
-playerSelection = playerSelection.toLowerCase();
-console.log(`Player chooses ${playerSelection}`);
-console.log(`Computer chooses ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+game();
